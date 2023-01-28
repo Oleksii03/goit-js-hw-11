fa1320bd21de2b3f71eece520d17946f89683c66
Oleksii_Pivniev <aapd8896@gmail.com>
2023-01-28 16:44:43 +0200

    fix/1-3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './sass/main.scss';
 import { fetchImages } from './js/fetch-images';
-import { renderGallery } from './js/render-gallery';
+import { renderGallery } from './js/gallery';
 import { onScroll, onToTopBtn } from './js/scroll';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
